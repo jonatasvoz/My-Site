74dedc184178e8fc90b4db515c269ef3040f1ef4
jonatasvoz <jonatasadamsvoz@gmail.com>
2024-12-15 21:11:44 -0300

    Adicionado Meta Tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <div>Aguardem, novidades estão por vir. Obrigado :-D</div>
+    return (
+    <div>
+        <head>
+            <meta name="facebook-domain-verification" content="c4gax5tztc4nqngxxwbj5f65tfe7un" />
+        </head>
+        <div>Aguardem, novidades estão por vir. Obrigado :-D</div>
+    </div>
+    )
 }
 
 export default Home
