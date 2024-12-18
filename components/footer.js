7ed8b5d9ad1048ade646b7142c9d5e137cea76ba
jonatasvoz <jonatasadamsvoz@gmail.com>
2024-12-18 00:10:04 -0300

    Adicionado mais coisas no footer

--- components/footer.js
+++ components/footer.js
@@ -3,10 +3,28 @@ import Link from 'next/link';
 const Footer = () => {
     return (
       <footer className="bg-gray-800 text-white py-6">
-        <nav>
-            <Link href="/politica-privacidade">Políticas de Privacidade e Termos de Uso</Link>
-        </nav>
-        <div className="container mx-auto text-center">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* Contato */}
+          <div>
+            <h3 className="text-xl font-bold mb-2">Contato</h3>
+            <p>Email: jonatas@email.com</p>
+            <p>Telefone: (11) 98765-4321</p>
+          </div>
+        {/* Links rápidos */}
+          <div>
+            <h3 className="text-xl font-bold mb-2">Links Rápidos</h3>
+              <ul>
+                <li><a href="/" className="hover:text-blue-400">Home</a></li>
+                <li><a href="/portfolio" className="hover:text-blue-400">Portfólio</a></li>
+                <li><a href="/blog" className="hover:text-blue-400">Blog</a></li>
+              </ul>
+          </div>
+          <div>
+            <h3 className="text-xl font-bold mb-2">Termos de Uso</h3>
+            <ul>
+              <li><a href="/politica-privacidade">Privacidade</a></li>
+            </ul>
+          </div>
           <p>&copy; {new Date().getFullYear()} Jonatas Adams CNPJ 47.028.450/0001-56. Todos os direitos reservados.</p>
         </div>
       </footer>
