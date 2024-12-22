b2aad366d882fc1e555acdb9dd1808777430edb6
jonatasvoz <jonatasadamsvoz@gmail.com>
2024-12-22 10:55:00 -0300

    Alteracoes e adicoes na home e portfolio

--- components/footer.js
+++ components/footer.js
@@ -1,34 +1,32 @@
-import Link from 'next/link';
-
 const Footer = () => {
-    return (
-      <footer className="bg-gray-800 text-white py-6">
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Contato */}
-          <div>
-            <h3 className="text-xl font-bold mb-2">Contato</h3>
-            <p>Email: jonatas@email.com</p>
-            <p>Telefone: (11) 98765-4321</p>
-          </div>
-        {/* Links rápidos */}
-          <div>
-            <h3 className="text-xl font-bold mb-2">Links Rápidos</h3>
-              <ul>
-                <li><a href="/" className="hover:text-blue-400">Home</a></li>
-                <li><a href="/portfolio" className="hover:text-blue-400">Portfólio</a></li>
-                <li><a href="/blog" className="hover:text-blue-400">Blog</a></li>
-              </ul>
-          </div>
-          <div>
-            <h3 className="text-xl font-bold mb-2">Termos de Uso</h3>
-            <ul>
-              <li><a href="/politica-privacidade">Privacidade</a></li>
-            </ul>
-          </div>
-          <p>&copy; {new Date().getFullYear()} Jonatas Adams CNPJ 47.028.450/0001-56. Todos os direitos reservados.</p>
+        <div>
+          <h3 className="text-xl font-bold mb-2">Contato</h3>
+          <p>Email: jonatas@email.com</p>
+          <p>Telefone: (11) 98765-4321</p>
         </div>
-      </footer>
-    );
-  };
-  
+        {/* Links rápidos */}
+        <div>
+          <h3 className="text-xl font-bold mb-2">Links Rápidos</h3>
+          <ul>
+            <li><a href="/" className="hover:text-blue-400">Home</a></li>
+            <li><a href="/portfolio" className="hover:text-blue-400">Portfólio</a></li>
+            <li><a href="/blog" className="hover:text-blue-400">Blog</a></li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-xl font-bold mb-2">Termos de Uso</h3>
+          <ul>
+            <li><a href="/politica-privacidade">Privacidade</a></li>
+          </ul>
+        </div>
+        <p>&copy; {new Date().getFullYear()} Jonatas Adams CNPJ 47.028.450/0001-56. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,61 +1,78 @@
 const Portfolio = () => {
     return (
         <section className="bg-gray-100 py-10">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-bold text-center mb-8">
+            <div className="container mx-auto">
+                <h2 className="text-3xl font-bold text-center mb-8">
                     Meu Portfólio
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h3 className="bg-white p-4 rounded-lg shadow-md">Demo Comercial</h3>
-                            <audio controls className="w-full">
-                                <source src="/media/democomercial.mp3" type="audio/mpeg" />
-                                Seu navegador não suporta a reprodução de áudio.
-                            </audio>
-                            <p className="text-sm text-gray-600 mt-2">Esse é meu Demonstrativo de Voz - Comercial</p>
-                        </div>
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h3 className="font-semibold mb-2">Anhanguera</h3>
-                            <audio controls className="w-full">
-                                <source src="/media/anhanguera.mp3" type="audio/mpeg" />
-                                Seu navegador não suporta a reprodução de áudio.
-                            </audio>
-                        </div>
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h3 className="font-semibold mb-2">Bulgari</h3>
-                            <audio controls className="w-full">
-                                <source src="/media/bulgari.mp3" type="audio/mpeg" />
-                                Seu navegador não suporta a reprodução de áudio.
-                            </audio>
-                        </div>
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h3 className="font-semibold mb-2">Nike</h3>
-                            <audio controls className="w-full">
-                                <source src="/media/nike.mp3" type="audio/mpeg" />
-                                Seu navegador não suporta a reprodução de áudio.
-                            </audio>
-                        </div>
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h3 className="font-semibold mb-2">Tim Black</h3>
-                            <audio controls className="w-full">
-                                <source src="/media/timblack.mp3" type="audio/mpeg" />
-                                Seu navegador não suporta a reprodução de áudio.
-                            </audio>
-                        </div>
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h3 className="font-semibold mb-2">Yoki</h3>
-                            <audio controls className="w-full">
-                                <source src="/media/yoki.mp3" type="audio/mpeg" />
-                                Seu navegador não suporta a reprodução de áudio.
-                            </audio>
-                        </div>
-                        <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-                            <h3 className="text-xl font-bold mb-2">Projeto 2</h3>
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/zQKthgvH9gY?si=9BJHdGDzBmg3Z4FK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                        </div>
+                </h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                        <h3 className="font-semibold mb-2">Demo Comercial</h3>
+                        <audio controls className="w-full">
+                            <source src="/media/democomercial.mp3" type="audio/mpeg" />
+                            Seu navegador não suporta a reprodução de áudio.
+                        </audio>
+                        <p className="text-sm text-gray-600 mt-2">Experiência em locução comercial para rádio, televisão e internet, com habilidade em transmitir emoção e persuasão através da voz. </p>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                        <h3 className="font-semibold mb-2">Anhanguera</h3>
+                        <audio controls className="w-full">
+                            <source src="/media/anhanguera.mp3" type="audio/mpeg" />
+                            Seu navegador não suporta a reprodução de áudio.
+                        </audio>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                        <h3 className="font-semibold mb-2">Bulgari</h3>
+                        <audio controls className="w-full">
+                            <source src="/media/bulgari.mp3" type="audio/mpeg" />
+                            Seu navegador não suporta a reprodução de áudio.
+                        </audio>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                        <h3 className="font-semibold mb-2">Nike</h3>
+                        <audio controls className="w-full">
+                            <source src="/media/nike.mp3" type="audio/mpeg" />
+                            Seu navegador não suporta a reprodução de áudio.
+                        </audio>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                        <h3 className="font-semibold mb-2">Tim Black</h3>
+                        <audio controls className="w-full">
+                            <source src="/media/timblack.mp3" type="audio/mpeg" />
+                            Seu navegador não suporta a reprodução de áudio.
+                        </audio>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                        <h3 className="font-semibold mb-2">Yoki</h3>
+                        <audio controls className="w-full">
+                            <source src="/media/yoki.mp3" type="audio/mpeg" />
+                            Seu navegador não suporta a reprodução de áudio.
+                        </audio>
+                    </div>
+                    <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+                        <h3 className="text-xl font-bold mb-2">Homenagens</h3>
+                        <iframe width="460" height="315" src="https://www.youtube.com/embed/zQKthgvH9gY?si=9BJHdGDzBmg3Z4FK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p class="body">Emocione-se com uma locução dedicada a momentos
+                            especiais. Com voz cativante e sensibilidade, entregamos
+                            homenagens únicas que eternizam memórias
+                            preciosas.</p>
+                    </div>
+                    <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+                        <h3 className="text-xl font-bold mb-2">Dublagem</h3>
+                        <iframe width="460" height="315" src="https://www.youtube.com/embed/GaYQwIwYaCE?si=3rH1UM8itSx9aJPC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p class="body">
+                            Minha capacidade de adaptação e interpretação permite que
+                            eu me encaixe perfeitamente no papel, trazendo vida e
+                            personalidade aos personagens.
+                        </p>
+                    </div>
+                    <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+                        <h3 className="text-xl font-bold mb-2">Projeto 2</h3>
+                        <iframe width="460" height="315" src="https://www.youtube.com/embed/zQKthgvH9gY?si=9BJHdGDzBmg3Z4FK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
     );
 };
 
